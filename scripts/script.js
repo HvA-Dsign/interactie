864dd45625290d667f46d09af643e550b335cbbd
HvA-Dsign <118122098+HvA-Dsign@users.noreply.github.com>
2023-11-03 12:05:53 +0100

    JS - toggle kwal bel

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
 // JavaScript Document
 console.log("Howdy!");
+
+var kwal1 = document.querySelector ("li:nth-of-type(1) img");
+var tekstBel1 = document.getElementById("bel1");
+
+kwal1.onclick = zichtbaarBel1;
+
+function zichtbaarBel1(){
+    tekstBel1.classList.toggle("bel1zichtbaar");
+    console.log("Howdy!");
+
+  }
