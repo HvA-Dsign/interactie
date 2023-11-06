7c180d7e843d26aa91078e04544fe136ef0e13fe
HvA-Dsign <118122098+HvA-Dsign@users.noreply.github.com>
2023-11-06 10:19:52 +0100

    back-up

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,40 @@
 // JavaScript Document
 console.log("Howdy!");
 
+// bel 1 
 var kwal1 = document.querySelector ("li:nth-of-type(1) img");
 var tekstBel1 = document.getElementById("bel1");
+var bel1 = document.querySelector ("#bel1");
 
-kwal1.onclick = zichtbaarBel1;
+function toggleBel1(){
+    tekstBel1.classList.toggle("belZichtbaar");
+    console.log("bel 1 zichtbaar");
+}
 
-function zichtbaarBel1(){
-    tekstBel1.classList.toggle("bel1zichtbaar");
-    console.log("Howdy!");
+function sluitBel1() {
+    if (tekstBel1.classList.contains("belZichtbaar")) {
+        tekstBel1.classList.remove("belZichtbaar");
+    }
+}
 
-  }
+kwal1.onclick = toggleBel1;
+bel1.onclick = sluitBel1;
+
+// bel 2
+var kwal2 = document.querySelector ("li:nth-of-type(2) img");
+var tekstBel2 = document.getElementById("bel2");
+var bel2 = document.querySelector ("#bel2");
+
+function toggleBel2(){
+    tekstBel2.classList.toggle("belZichtbaar");
+    console.log("bel 2 zichtbaar");
+}
+
+function sluitBel2() {
+    if (tekstBel2.classList.contains("belZichtbaar")) {
+        tekstBel2.classList.remove("belZichtbaar");
+    }
+}
+
+kwal2.onclick = toggleBel2;
+bel2.onclick = sluitBel2;
